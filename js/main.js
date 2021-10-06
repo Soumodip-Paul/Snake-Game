d13c5099de82f5f57e244e1af516fa7c3f0efc5c
Soumodip Paul <soumodippaul10032002@outlook.in>
2021-10-06 23:57:10 +0530

    recreated snake-game using div

--- js/main.js
+++ js/main.js
@@ -1,119 +1,147 @@
-//constants
-let inputDir= {x:0 , y:0};
-let food ={x:12,y:13}
-let lastPaintTime = 0;
-let snakeArr = [{x :13,y:15}]; 
-const speed =06;
+let board = document.getElementById('board');
+let a = 1, b = 30;
+board.style.gridTemplateColumns = `repeat(${b}, 1fr)`
+board.style.gridTemplateRows = `repeat(${b}, 1fr)`
+
+let snakeArray = [{x: 7, y: 5}, {x:7,y:4}]
+let food = {x:5,y:5}
+let movingDirection = {x:0 , y: 1}
 let score = 0;
-let a=0,b=50;
-// const board = document.querySelector("#board"); 
-// let foodSound = new Audio('assets/food.mp3');
-// let gameoverSound = new Audio('assets/gameover.mp3');
-// let moveSound = new Audio('assets/move.mp3');
-// let backSound = new Audio('assets/backGround.mp3');
+let highScore = localStorage.getItem('highest-score') || 0;
+let lastPaintTime = 0;
+let speed = 05;
+let requestId = null
+let foodElem;
+let snakePart
 
-//functions
-function main(ctime){
+document.querySelector("#high").innerHTML = highScore
+document.querySelector("#score").innerHTML = score
 
-    window.requestAnimationFrame(main);
-    
-    if((ctime - lastPaintTime)/1000 < 1/speed) return;
-    // console.log(ctime);
-    lastPaintTime = ctime;
-    gameEngine();
-    
+// Creating Animation
+function loop(time) {
+    requestId =  window.requestAnimationFrame(loop)
+    if ((time - lastPaintTime) / 1000 < 1 / speed ) return;
+    lastPaintTime = time
+    gameEngine()
 }
 
-function gameEngine(){
-    //Part 1: Updating snake array
-    if(isCollide(snakeArr)){
-        //gameOver.play()
-        //music.pause()
-        inputDir= {x:0 , y:0};
-        alert("Game Over"+score);
-        snakeArr = [{x :13,y:15}]; 
-        //music.play
-        score =0;
-
-    }
-    //if you eat food
-
-    if(snakeArr[0].x===food.x&&snakeArr[0].y===food.y){
-        snakeArr.unshift({x:snakeArr[0].x+inputDir.x,y:snakeArr[0].y+inputDir.y});
-        food = {x:Math.round(a+(b-a)*Math.random()),y:Math.round(a+(b-a)*Math.random())}
-        
-    }
-    // move the sanke
-    for (let index = snakeArr.length -2; index >=0 ; index--) {
-        const element = snakeArr[index];
-        snakeArr[index+1] ={...snakeArr[index]};
-        
-    }
-
-    snakeArr[0].x += inputDir.x;
-    snakeArr[0].y += inputDir.y;
-
-
-    //Part 2: Display snake array and food
-    board.innerHTML = '';
-    snakeArr.forEach((e,index)=>{
-
-        snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = e.y;
-        snakeElement.style.gridColumnStart = e.x;
-        snakeElement.classList.add('snake');
-        if(index === 0) snakeElement.classList.add('head');
-        board.appendChild(snakeElement);
-    });
-
-    //Display food
-    foodElement = document.createElement('div');
-    foodElement.style.gridRowStart = food.y;
-    foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('food');
-    board.appendChild(foodElement);
-
-}
-
-function isCollide(sarr){
-    for (let i = 1; i < snakeArr.length; i++) {
-        if (snakeArr[i].x === snakeArr[0].x&&snakeArr[i].y===snakeArr[0].y){
-            return true;
+function gameEngine() {
+    // PART 1:  Adding logic to eat food and move
+    snakeArray.forEach((e , i)=> {
+        if (i !== 0 && snakeArray[0].x === e.x && snakeArray[0].y === e.y) {
+            alert(`Game over your score is ${score}`)
+            snakeArray = [{x: 7, y: 5},{x:7,y:4}];
+            food = {x : generateRnadom(b,a), y: generateRnadom(b,a)}
+            movingDirection = {x:0,y:1} 
+            setScore(0)
         }
+    })
 
-        
-    }
+    // Snake eat food 
+
+    if (snakeArray[0].x === food.x && snakeArray[0].y === food.y) snakeHaveEatenFood(snakeArray)
+
+    // Move the snake 
+
+    for (let index = snakeArray.length - 2; index >=0 ; index--) 
+        {snakeArray[ index + 1] = {...snakeArray[index]}}
+
+    // snake is moving
+
+    snakeArray[0].x += movingDirection.x
+    snakeArray[0].y += movingDirection.y
+
+    // wraping the snake at wall
+
+    if (snakeArray[0].x > b) snakeArray[0].x -= b
+    if (snakeArray[0].x < a ) snakeArray[0].x = b - snakeArray[0].x
+    if (snakeArray[0].y > b) snakeArray[0].y -= b
+    if (snakeArray[0].y < a ) snakeArray[0].y = b - snakeArray[0].y
+
+    // PART 2 : Creating snake and food
+    drawSnakeAndFood()
+    
 }
 
+function snakeHaveEatenFood(sarr) { 
+    sarr.unshift({x : sarr[0].x + movingDirection.x, y: sarr[0].y + movingDirection.y})
+    setScore(speed + 5)    
+    food = {x: generateRnadom(b,a), y: generateRnadom(b,a)}
+}
 
-window.requestAnimationFrame(main);
+function generateRnadom(max,min) { 
+    return Math.round(min + (max - min ) * Math.random())
+}
 
-window.addEventListener('keydown',e=>{
-    inputDir= {x:0,y:1}//Start the game
+function setScore(sc) {
+    score = sc === 0 ? sc : score + sc
+    highScore = highScore >= score ? highScore : score 
+    localStorage.setItem('highest-score',highScore)
+    document.querySelector("#high").innerHTML = highScore
+    document.querySelector("#score").innerHTML = score
+}
+
+// requestId =  window.requestAnimationFrame(loop)
+drawSnakeAndFood()
+
+function drawSnakeAndFood() {
+    board.innerHTML = ''
+
+    // creating snake 
+    snakeArray.forEach((e,index) => {
+        snakePart = document.createElement('div')
+        snakePart.classList.add('snake')
+        snakePart.style.gridRowStart = e.y
+        snakePart.style.gridColumnStart = e.x
+        if (index === 0) snakePart.classList.add('head')
+        board.appendChild(snakePart)
+    })
+
+    // creating food
+
+    foodElem = document.createElement('div')
+    foodElem.style.gridRowStart = food.y
+    foodElem.style.gridColumnStart = food.x
+    foodElem.classList.add('food')
+    board.appendChild(foodElem)
+}
+
+// Toogle gameplay
+
+function playPauseGame(e) {
+    if(requestId !== null && e==='p' ) {
+        window.cancelAnimationFrame(requestId)
+        requestId = null
+    }
+    else if(requestId === null) requestId = window.requestAnimationFrame(loop)
+    else {}
+}
+
+window.addEventListener('keydown', e => {
+    //Start the game
     //moveSound.play()
     switch (e.key) {
         case "ArrowUp":
-            console.log(e.key);
-            inputDir.x = 0;
-            inputDir.y = -1;
+            movingDirection.x = 0;
+            movingDirection.y = movingDirection.y === 1 ? 1 : -1;
             break;
         case "ArrowLeft":
-            console.log(e.key);
-            inputDir.x = -1;
-            inputDir.y = 0;
+            movingDirection.x = movingDirection.x === 1 ? 1 : -1;
+            movingDirection.y = 0;
             break;
         case "ArrowDown":
-            console.log(e.key);
-            inputDir.x = 0;
-            inputDir.y = 1;
+            movingDirection.x = 0;
+            movingDirection.y = movingDirection.y === -1 ? -1 : 1;
             break;
         case "ArrowRight":
-            console.log(e.key);
-            inputDir.x = 1;
-            inputDir.y = 0;
+            movingDirection.x = movingDirection.x === -1 ? -1 : 1;
+            movingDirection.y = 0;
+            break;
+        case 'p': 
+            playPauseGame(e.key)
             break;
         default:
+            playPauseGame(null)
             break;
     }
-
-});
+})
